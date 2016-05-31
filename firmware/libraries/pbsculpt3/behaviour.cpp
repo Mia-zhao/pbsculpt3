31e1c646c336df362b6b4c7d4b24a99efa4dd615
David Kadish <david.kadish@gmail.com>
2016-05-31 09:18:18 +0200

    Switched to integer-based behaviours
    Removed floating-point functionality to speed up calculations.

--- firmware/libraries/pbsculpt3/behaviour.cpp
+++ firmware/libraries/pbsculpt3/behaviour.cpp
@@ -7,12 +7,134 @@
 
 #include "behaviour.h"
 
-Behaviour::Behaviour() {
-	// TODO Auto-generated constructor stub
+Behaviour::Behaviour():
+	_runTime(0),
+	_pauseTime(0),
+	_maxValue(255),
+	_timeMux(1),
+	_timeDiv(1),
+	_valueMux(1),
+	_valueDiv(1),
+	_state(STOPPED),
+	_i_lastPoint(0)
+	{
 
+    points = LinkedList<Long*>();
 }
 
 Behaviour::~Behaviour() {
 	// TODO Auto-generated destructor stub
 }
 
+void Behaviour::loop(){
+	switch(_state){
+	case PLAYING:
+		// Check to see if we should move forward to the next control point?
+		if( _runTime > nextPointStartTime() ){
+			// Move forward one point, but if this is the end, break.
+			if(!advancePoint()){
+				_state = STOPPED;
+			}
+		}
+		break;
+	}
+}
+
+int Behaviour::value(){
+	switch(_state){
+	case PLAYING:
+		long t_1 = _lastPoint[0], t_2 = _nextPoint[0],
+			 v_1 = _lastPoint[1], v_2 = _lastPoint[1];
+
+		long v_d = v_2 - v_1, t_d = t_2 - t_1;
+
+		return v_1 + v_d * (_runTime - t_1) / t_d;
+	case STOPPED:
+		return 0;
+	case PAUSED:
+		return 0;
+	}
+}
+
+void Behaviour::play(){
+	switch(_state){
+	case STOPPED:
+		_runTime = 0;
+		_i_lastPoint = 0;
+		break;
+	case PAUSED:
+		_runTime = _runTime - _pauseTime; // @TODO: test this
+		break;
+	}
+	_state = PLAYING;
+}
+
+void Behaviour::stop(){
+	_state = STOPPED;
+}
+
+void Behaviour::pause(){
+	switch(_state){
+	case PLAYING:
+		_pauseTime = 0;
+		break;
+	case STOPPED:
+
+	}
+	_state = PAUSED;
+}
+
+void Behaviour::reset(){
+	_runTime = 0;
+	_i_lastPoint = 0;
+}
+
+// Set limitations for behaviour
+void Behaviour::setMaxValue(long value){
+	_maxValue = value;
+}
+
+// Set multipliers and dividers
+void Behaviour::setTimeMultiplier(int mux){
+	// @TODO: Implement a non-stopped version
+	switch(_state){
+	case STOPPED:
+		_timeMux = mux;
+	}
+}
+
+void Behaviour::setTimeDivisor(int div){
+	// @TODO: Implement a non-stopped version
+	switch(_state){
+	case STOPPED:
+		_timeDiv = div;
+	}
+}
+
+void Behaviour::setValueMultiplier(int mux){
+	_valueMux = mux;
+}
+
+void Behaviour::setValueDivisor(int div){
+	_valueDiv = div;
+}
+
+// @TODO: FINISH THIS!
+bool Behaviour::advancePoint(){
+	_i_lastPoint += 1;
+	if( _i_lastPoint < points.size() ){
+		_lastPoint = points.get(_i_lastPoint);
+		_nextPoint = points.get(_i_lastPoint+1);
+		return true;
+	} else {
+		_i_lastPoint = 0;
+		_lastPoint = points.get(_i_lastPoint);
+		if ( points.size() > 1 ){
+			_nextPoint = points.get(_i_lastPoint+1);
+		} else {
+			_nextPoint = 0;
+		}
+		return false;
+	}
+
+}
--- firmware/libraries/pbsculpt3/behaviour.h
+++ firmware/libraries/pbsculpt3/behaviour.h
@@ -2,10 +2,23 @@
  *
  *  Created on: May 27, 2016
  *      Author: dkadish
+ *
+ *
+ *  @todo: What if this was reformulated with time being the delay in microseconds
+ *  	   from the previous action in the default setting. Which could then get
+ *  	   stretched and shrunk. MUCH EASIER MATH!
  */
 
 #ifndef BEHAVIOUR_H_
 #define BEHAVIOUR_H_
+
+#include <LinkedList.h>
+
+enum BehaviourState {
+	STOPPED,
+	PLAYING,
+	PAUSED
+};
 
 class Behaviour {
 public:
@@ -18,19 +31,61 @@ public:
 	 *	@description Currently, the module performs a simple linear interpolation
 	 *				 between points, but in the future, this may shift to a more
 	 *				 complex (Hermite, Catmull-Rom) spline-based interpolation.
+	 *				 NOTE: Behaviours MUST have a start point and an end point
+	 *				 and will fail with LESS than two points.
 	 *
 	 */
 	Behaviour();
 	virtual ~Behaviour();
 
-	void start();
+	// Integration functions
+	void loop();
+	int value();
+
+	// Control functions
+	void play();
 	void stop();
 	void pause();
 	void reset();
 
+	// Set limitations for behaviour
+	void setMaxValue( long value );
+
+	// Set multipliers and dividers
+	void setTimeMultiplier( int mux );
+	void setTimeDivisor( int div );
+	void setValueMultiplier( int mux);
+	void setValueDivisor(int div);
+
+    LinkedList<long*> points;
+
 private:
+    long currentPointStartTime();
+    long nextPointStartTime();
+
+    /** Advance to the next set of control points.
+     *
+     *	@return Boolean indicating whether there are more points in the behaviour definition.
+     */
+    bool advancePoint();
+
 	elapsedMillis _runTime;
 	long _pauseTime;
+
+	/** Pointer towards a 2D array of points in the format [time, value]
+	 *
+	 *	@description
+	 */
+	long _maxValue;
+
+	int _timeMux, _timeDiv;
+	int _valueMux, _valueDiv;
+
+	// Playing variables
+	BehaviourState _state;
+	int _i_lastPoint;
+
+	long *_lastPoint, *_nextPoint;
 };
 
 #endif /* BEHAVIOUR_H_ */
