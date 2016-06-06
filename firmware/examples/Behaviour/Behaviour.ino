e89eb72fe610abb95979d74d6f9de80a65935f3e
David Kadish <david.kadish@gmail.com>
2016-06-06 14:35:35 +0200

    Documentation of behaviours and slight changes to the example
    Completed full documentation of the behaviour modules and made a couple of minor shifts to the instantiation of  behaviour in the example.

--- firmware/examples/Behaviour/Behaviour.ino
+++ firmware/examples/Behaviour/Behaviour.ino
@@ -4,62 +4,53 @@
 
 Behaviour exponentialBehaviour;
 
+// The number of points
+const int n_timeValue = 18;
+
+// Timings in microseconds
+unsigned long timeValue[n_timeValue][2] = {
+		{0L, 0L},
+		{100000L, 30L},
+		{200000L, 86L},
+		{300000L, 134L},
+		{400000L, 174L},
+		{500000L, 206L},
+		{600000L, 230L},
+		{700000L, 246L},
+		{800000L, 254L},
+		{900000L, 254L},
+		{1000000L, 246L},
+		{1100000L, 230L},
+		{1200000L, 206L},
+		{1300000L, 174L},
+		{1400000L, 134L},
+		{1500000L, 86L},
+		{1600000L, 30L},
+		{1700000L, 0L}
+};
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
 	Serial.begin(9600);
 	delay(1000); // Wait for serial to start
 
-	Serial.println("EXAMPLE: Starting Firmware...");
 	// Set up test LED
-
-	Serial.println("EXAMPLE: Setting LED mode...");
 	pinMode(LED_BUILTIN, OUTPUT);
 
-	Serial.println("EXAMPLE: Beginning SoftPWM...");
 	SoftPWMBegin();
-
 	SoftPWMSet(LED_BUILTIN, 0);
 
-	Serial.println("EXAMPLE: Initializing behaviour...");
+	Serial.println("Initializing behaviour...");
 	exponentialBehaviour = Behaviour();
 
-	const int n_timeValue = 18;
-
-	// Timings in microseconds
-	unsigned long timeValue[n_timeValue][2] = {
-			{0L, 0L},
-			{100000L, 30L},
-			{200000L, 86L},
-			{300000L, 134L},
-			{400000L, 174L},
-			{500000L, 206L},
-			{600000L, 230L},
-			{700000L, 246L},
-			{800000L, 254L},
-			{900000L, 254L},
-			{1000000L, 246L},
-			{1100000L, 230L},
-			{1200000L, 206L},
-			{1300000L, 174L},
-			{1400000L, 134L},
-			{1500000L, 86L},
-			{1600000L, 30L},
-			{1700000L, 0L}
-	};
-	Point* points[n_timeValue];
 	for( int i = 0; i < n_timeValue; i++ ){
-		points[i] = new Point();
-		points[i]->time = timeValue[i][0];
-		points[i]->value = timeValue[i][1];
+		Point *point = new Point();
+		point->time = timeValue[i][0];
+		point->value = timeValue[i][1];
+
+		exponentialBehaviour.points.add(point);
 	}
-
-
-	for( int i = 0; i < n_timeValue; i++ ){
-		exponentialBehaviour.points.add(points[i]);
-	}
-
-	Serial.println("EXAMPLE: Finished boot sequence...");
 
 	exponentialBehaviour.play();
 }
--- firmware/libraries/pbsculpt3/behaviour.h
+++ firmware/libraries/pbsculpt3/behaviour.h
@@ -17,54 +17,158 @@
 #include <LinkedList.h>
 #include <elapsedMillis.h>
 
+/** States for the behaviour
+ *
+ * 	@brief Defines the behaviour state as stopped, playing, or paused.
+ *
+ * 	@details Uses a media player analogy and allows a behaviour to be in one of three states.
+ */
 enum BehaviourState {
-	STOPPED,
-	PLAYING,
-	PAUSED
+	STOPPED,//!< Not playing, reset to the beginning
+	PLAYING,//!< Playing
+	PAUSED  //!< Stopped, but position is kept
 };
 
+/** Point on the behaviour plot
+ *
+ * 	@brief Charts a point on the time-value (x,y) plot for a behaviour.
+ *
+ * 	@details Both are stored as unsigned long's to simplify creation. The points
+ * 			 must be added in chronological order. Any points out of order will
+ * 			 be ignored.
+ *
+ */
 struct Point {
-	unsigned long time;
-	unsigned long value;
+	unsigned long time; //!< Time (in microseconds) at the behaviour point
+	unsigned long value; //!< Value (usually between 0-255) at behaviour point
 };
 
+/**	Stores a specific behaviour for a specific actuator in the pbsculpt system
+ *
+ *	@brief A behaviour is defined by a set of control points that are passed
+ *	       over a span of time.
+ *
+ *	@details Currently, the module performs a simple linear interpolation
+ *			 between points, but in the future, this may shift to a more
+ *			 complex (Hermite, Catmull-Rom) spline-based interpolation.
+ *			 NOTE: Behaviours MUST have a start point and an end point
+ *			 and will fail with LESS than two points.
+ *
+ */
 class Behaviour {
 public:
 
-	/** Stores a specific behaviour for a specific actuator in the pbsculpt system
-	 *
-	 *	@brief A behaviour is defined by a set of control points that are hit
-	 *	       over a span of time.
-	 *
-	 *	@description Currently, the module performs a simple linear interpolation
-	 *				 between points, but in the future, this may shift to a more
-	 *				 complex (Hermite, Catmull-Rom) spline-based interpolation.
-	 *				 NOTE: Behaviours MUST have a start point and an end point
-	 *				 and will fail with LESS than two points.
-	 *
-	 */
 	Behaviour();
 	virtual ~Behaviour();
 
 	// Integration functions
+	/** Call this in the loop() function of the .ino script to keep the
+	 * 	behaviour up to date.
+	 *
+	 */
 	void loop();
+
+	/** Get the current value of the behaviour
+	 *
+	 * @return The Integer value of the current behaviour (default: [0-255])
+	 */
 	int value();
 
 	// Control functions
+	/** Start (or continue) the behaviour
+	 *
+	 */
 	void play();
+
+	/** Stop and reset the behaviour
+	 *
+	 */
 	void stop();
+
+	/** Pause the behaviour
+	 *
+	 */
 	void pause();
+
+	/** Reset the behaviour to the beginning
+	 *
+	 * If the behaviour is playing it will continue to play, just from the start.
+	 * If it is stopped or paused, it will revert to the beginning, but will not
+	 * play until play() is called.
+	 */
 	void reset();
 
-	// Set limitations for behaviour
+	/**	Set the maximum value for behaviour
+	 *
+	 * The ceiling will be enforced so that if the value rises above the max,
+	 * it will be capped at this number.
+	 *
+	 * @param value The maximum value that the behaviour should output.
+	 */
 	void setMaxValue( unsigned long value );
 
-	// Set multipliers and dividers
+	/** @name Scaling Functions
+	 *
+	 * Because the behaviour system uses integer math, the scaling factors have
+	 * to be set as separate multipiers and divisors. This goes for both the
+	 * time and values. So, for example, to increase the length of time that
+	 * the behaviour takes, you should run
+	 * @code
+	 * behaviour.setTimeMultiplier(3);
+	 * behaviour.setTimeDivisor(2);
+	 * @endcode
+	 * which will lead to a multiplication of the behaviour time by 1.5.
+	 */
+	//@{
+	/** Set the multiplier for the time in order to stretch the behaviour
+	 *
+	 * @param mux Multiplication factor
+	 */
 	void setTimeMultiplier( int mux );
-	void setTimeDivisor( int div );
-	void setValueMultiplier( int mux);
-	void setValueDivisor(int div);
 
+	/** Set the divisor for the time in order to compress the behaviour
+	 *
+	 * @param div Division factor
+	 */
+	void setTimeDivisor( int div );
+
+	/** Set the multiplier for the value in order to compress the behaviour
+	 *
+	 * @param mux Multiplication factor
+	 */
+	void setValueMultiplier( int mux);
+
+	/**Set the divisor for the time in order to compress the behaviour
+	 *
+	 * @param div Division factor
+	 */
+	void setValueDivisor(int div);
+	//@}
+
+	/** LinkedList of Points (time, value) that the behaviour will pass through
+	 *
+	 * 	This LinkedList must be populated (in the order of time) with
+	 * 	Points that will be passed through for the behaviour. At least
+	 * 	two points must be specified.
+	 *
+	 *	For example:
+	 * 	@code
+	 * 	Behaviour behaviour = Behaviour();
+	 *	unsigned long timeValue[n_timeValue][2] = {
+	 *		{0L, 0L},
+	 *		{100000L, 30L},
+	 *	};
+	 *
+	 *	for( int i = 0; i < n_timeValue; i++ ){
+	 *		Point *point = new Point();
+	 *		point->time = timeValue[i][0];
+	 *		point->value = timeValue[i][1];
+	 *
+	 *		behaviour.points.add(point);
+	 *	}
+	 * 	@endcode
+	 *
+	 */
     LinkedList<Point*> points;
 
 private:
@@ -81,10 +185,6 @@ private:
 	elapsedMicros _runTime;
 	unsigned long _pauseTime;
 
-	/** Pointer towards a 2D array of points in the format [time, value]
-	 *
-	 *	@description
-	 */
 	unsigned long _maxValue;
 
 	int _timeMux, _timeDiv;
